3364da6fbebcfc38a076f272be612126f1c0bf86
Art Brown <artb@192.168.1.7>
2019-11-30 05:51:53 -0500

    Fix quouting input

--- csv_parser.py
+++ csv_parser.py
@@ -3,41 +3,58 @@
 import argparse
 import csv
 import copy
+import sys
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='parse column(s) from CSV file')
-    parser.add_argument('--outfile', help='output file; stdout if not given', default='/dev/stdout')
-    parser.add_argument('--infile', help='input file',default='/dev/stdin')
-    parser.add_argument('--no-header', action="store_true", default=False, dest='no_header', help='skip headers in output')
-    parser.add_argument('cnames',  type=str, nargs='+', help='name(s) of header columns to parse')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="parse column(s) from CSV file")
+    parser.add_argument(
+        "--outfile", help="output file; stdout if not given", default="/dev/stdout"
+    )
+    parser.add_argument("--infile", help="input file", default="/dev/stdin")
+    parser.add_argument(
+        "--no-header",
+        action="store_true",
+        default=False,
+        dest="no_header",
+        help="skip headers in output",
+    )
+    parser.add_argument(
+        "cnames", type=str, nargs="+", help="name(s) of header columns to parse"
+    )
     args = parser.parse_args()
 
+    with open(args.outfile, mode="w") as out_csv:
+        csv_writer = csv.writer(out_csv, delimiter=",")
 
-    with open(args.outfile, mode='w') as  out_csv :
-        csv_writer = csv.writer(out_csv, delimiter=',')
-        
-        with open( args.infile, 'r') as in_csv :
+        with open(args.infile, "r") as in_csv:
             # get header
-            header = in_csv.readline().strip()
-            
+            csv_reader = csv.reader(in_csv, delimiter=",")
+            for header_list in csv_reader :
+                break
+            #header = csv_reader.next
+            #header = in_csv.readline().strip()
+            #print(header)
+            #sys.exit(0) 
+
             # create input header lookup dict
             lookup = {}
-            header_list = header.split(',')
+            #header_list = header.split(',')
+            #print(header_list)
+            #sys.exit(0) 
 
-            for pos, name in enumerate(header_list) :
+            for pos, name in enumerate(header_list):
                 lookup[name] = pos
-
+            print(lookup)
+            #print(lookup['"p_state"'])
             # print the output header line
-            if not args.no_header :
-               csv_writer.writerow(args.cnames)
-                
+            if not args.no_header:
+                csv_writer.writerow(args.cnames)
 
             # now get the actual data
-            csv_reader = csv.reader(in_csv, delimiter=',')
-            for  lineno, in_row in enumerate(csv_reader):
+            #csv_reader = csv.reader(in_csv, delimiter=",")
+            for lineno, in_row in enumerate(csv_reader):
                 out_row = []
-                for name in args.cnames :
-                   out_row.append(in_row[lookup[name]])
+                for name in args.cnames:
+                    out_row.append(in_row[lookup[name]])
                 csv_writer.writerow(out_row)
-
